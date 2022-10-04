cf4e7caea5b37587ee83f068e6ea256345e843e9
Hexade <85897199+superboom12s@users.noreply.github.com>
2022-10-04 23:34:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+import math
+import random
+
+
+continuar = input("Su procesador podria sobrecalentarse, continuar?\n[S/N]")
+
+if continuar == "s" or continuar == "s":
+
+    print("cargando...")
+
+    loopVal = 0
+
+    lagString = []
+
+    for i in range(99999999):
+
+        lagString.append(" ")
+
+    print("carga terminada")
+
+
+
+    while True:
+
+        actualLag = math.sqrt(math.sqrt(random.randint(0, 99999999)))
+
+        print(actualLag)
+
+        try:
+            lagString[loopVal] = actualLag
+            loopVal += 1
+        except:
+            loopVal = 0
+
+
+
+
+
+else:
+
+    print("Operacion cancelada")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
